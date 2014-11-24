71cfceeceb7e6e33c09a1e0fca4d6da23a5ba713
rbridges <rbridges@ucsd.edu>
2014-11-23 21:53:11 -0800

    Added some comments and did a small bugfix in TellerAccountPage.java

--- Project/Backend/CheckingAccount.java
+++ Project/Backend/CheckingAccount.java
@@ -48,15 +48,18 @@ public class CheckingAccount extends BankAccount
         // Check if this withdraw is less than 24 hours from the last withdraw
         if (Hours.hoursBetween(currentDateTime, lastWithdrawDateTime).getHours() <= 24)
         {
+            // Ensure that this withdraw will not put the user over their threshold
             if ((dayWithdrawAmount + amount) <= 10000)
             {
-                this.balance -= amount;             
+                // Perform withdraw and update threshold
+                this.balance -= amount;
                 dayWithdrawAmount += amount;
                 return true;
             }
         }
-        else if (Hours.hoursBetween(currentDateTime, lastWithdrawDateTime).getHours() > 24)
+        else // If it has been more that 24 hours since the last withdraw
         {
+            // Do the withdraw and update the lastWithdrawDateTime
             this.balance -= amount;
             lastWithdrawDateTime = currentDateTime;
             dayWithdrawAmount = amount;
@@ -64,7 +67,7 @@ public class CheckingAccount extends BankAccount
         }
         return false;
     }
-    
+
     @Override
     public boolean addToBalance(double amount)
     {
@@ -77,15 +80,18 @@ public class CheckingAccount extends BankAccount
         // Check if this deposit is less than 24 hours from the last deposit
         if (Hours.hoursBetween(currentDateTime, lastDepositDateTime).getHours() <= 24)
         {
+            // Check if this deposit will put the user over their daily threshold
             if ((dayDepositAmount + amount) <= 10000)
             {
+                // Update threshold and deposit the money
                 this.balance += amount;
                 dayDepositAmount += amount;
                 return true;
             }
         }
-        else if (Hours.hoursBetween(currentDateTime, lastDepositDateTime).getHours() > 24)
+        else // It has been more than 24 hrs since the last deposit
         {
+            // Update threshold, lastDepositDateTime and deposit the money
             this.balance += amount;
             lastDepositDateTime = currentDateTime;
             dayDepositAmount = amount;
