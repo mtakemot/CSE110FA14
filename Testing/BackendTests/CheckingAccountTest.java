e21df10ac53182fc2ed74098d74700ae86969d19
rbridges <rbridges@ucsd.edu>
2014-11-29 15:38:22 -0800

    PenaltyInterestPanel now shows when the teller presses the Penalty/Interest button from the main menu

--- Testing/BackendTests/CheckingAccountTest.java
+++ Testing/BackendTests/CheckingAccountTest.java
@@ -1,13 +1,9 @@
 package BackendTests;
 
-import LogicLayer.BankAccount;
 import LogicLayer.CheckingAccount;
-import LogicLayer.SavingsAccount;
-import LogicLayer.UserAccount;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeUtils;
 import org.joda.time.DateTimeZone;
-import static org.joda.time.Duration.millis;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -15,7 +11,7 @@ import static org.junit.Assert.*;
  *
  * @author Tahereh Masoumi This class tests the CheckingAccount class
  * functionality.
- * 
+ *
  */
 public class CheckingAccountTest
 {
@@ -36,9 +32,9 @@ public class CheckingAccountTest
     }
 
     /**
-     * ===Test 1 of addToBalance method, of class CheckingAccount=== general tests
-     * for depositing amount to a checking account, tests if balance reflects
-     * deposits correctly
+     * ===Test 1 of addToBalance method, of class CheckingAccount=== general
+     * tests for depositing amount to a checking account, tests if balance
+     * reflects deposits correctly
      */
     @Test
     public void testAddToBalance_General()
@@ -46,18 +42,18 @@ public class CheckingAccountTest
         instance.setBalance(0);
         System.out.println("Balance reflects deposits");
         double totalBal = 0;
-        for(int i =1; i < 10; i++)
+        for (int i = 1; i < 10; i++)
         {
-            double expResult = totalBal = instance.getBalance() + (i*10);
-            instance.addToBalance(i*10);
+            double expResult = totalBal = instance.getBalance() + (i * 10);
+            instance.addToBalance(i * 10);
             double result = instance.getBalance();
             assertEquals(expResult, result, .001);
         }
     }
-    
-       /**
-     * ===Test 2 of addToBalance method, of class CheckingAccount=== general tests
-     * daily deposit limits
+
+    /**
+     * ===Test 2 of addToBalance method, of class CheckingAccount=== general
+     * tests daily deposit limits
      */
     @Test
     public void testAddToBalance_DepositlessThanDailyLimit()
@@ -68,23 +64,24 @@ public class CheckingAccountTest
         boolean res1 = instance.addToBalance(5000);
         boolean exp1 = true;
         assertEquals(exp1, res1);
-    }    
-    
-           /**
-     * ===Test 3 of addToBalance method, of class CheckingAccount=== 
-     * testing edge case: $10000 balance
+    }
+
+    /**
+     * ===Test 3 of addToBalance method, of class CheckingAccount=== testing
+     * edge case: $10000 balance
      */
     @Test
     public void testAddToBalance_DepositEqualToDailyLimit()
-    {  
+    {
         System.out.println("Daily depositing equal to $10000 daily limit");
         boolean res2 = instance.addToBalance(10000);
         boolean exp2 = true;
         assertEquals(exp2, res2);
     }
+
     /**
-     * ===Test 3 of addToBalance method, of class CheckingAccount=== 
-     * testing depositing more than $10000 in a day
+     * ===Test 3 of addToBalance method, of class CheckingAccount=== testing
+     * depositing more than $10000 in a day
      */
     @Test
     public void testAddToBalance_DepositMoreThanDailyLimit()
@@ -94,33 +91,33 @@ public class CheckingAccountTest
         boolean exp3 = false;
         assertEquals(exp3, res3);
     }
-    
-    /**
-     * ===Test 4 of addToBalance method, of class CheckingAccount=== 
-     * testing depositing more than $10000 in a 2-day period
-     */
-    @Test    
-    public void testAddToBalance_DepositMoreThanLimit2DaysPeriod()
-    {   
-       System.out.println("initial balance: "+ instance.getBalance());
-       instance.addToBalance(9000);
-       System.out.println("balance after 9000 deposit in one day: "+ instance.getBalance());
 
-       //changing current time to one day after actual time
+    /**
+     * ===Test 4 of addToBalance method, of class CheckingAccount=== testing
+     * depositing more than $10000 in a 2-day period
+     */
+    @Test
+    public void testAddToBalance_DepositMoreThanLimit2DaysPeriod()
+    {
+        System.out.println("initial balance: " + instance.getBalance());
+        instance.addToBalance(9000);
+        System.out.println("balance after 9000 deposit in one day: " + instance.getBalance());
+
+        //changing current time to one day after actual time
         DateTimeUtils.setCurrentMillisOffset(100000000);
-        
+
         DateTime fake = new DateTime(DateTimeZone.forID("Etc/UTC"));
-        System.out.println("depositing more than $10000 in 2-days period " +fake.toString());
+        System.out.println("depositing more than $10000 in 2-days period " + fake.toString());
         boolean res4 = instance.addToBalance(5000);
-        System.out.println("balance after 5000 deposit in next day: "+ instance.getBalance());
+        System.out.println("balance after 5000 deposit in next day: " + instance.getBalance());
 
         boolean exp4 = true;
         assertEquals(exp4, res4);
     }
-    
+
     /**
-     * ===Test 5 of addToBalance method, of class CheckingAccount=== 
-     * testing depositing more than $10000 in a period more than 2 days
+     * ===Test 5 of addToBalance method, of class CheckingAccount=== testing
+     * depositing more than $10000 in a period more than 2 days
      */
     @Test
     public void testAddToBalance_DepositMoreThanLimitSeveralDaysPeriod()
@@ -128,16 +125,17 @@ public class CheckingAccountTest
         instance.addToBalance(6000);
         DateTimeUtils.setCurrentMillisOffset(200000000);
         DateTime fake2 = new DateTime(DateTimeZone.forID("Etc/UTC"));
-        System.out.println("depositing more than $10000 in several-days period " +
-                fake2.toString() + "balance"+instance.getBalance());
+        System.out.println("depositing more than $10000 in several-days period "
+                + fake2.toString() + "balance" + instance.getBalance());
         boolean res5 = instance.addToBalance(5000);
         boolean exp5 = true;
         assertEquals(exp5, res5);
-        
+
     }
+
     /**
-     * ===Test 1 of subFromeBalance method, of class CheckingAccount=== tests the
-     * withdraw of an amount more than balance
+     * ===Test 1 of subFromeBalance method, of class CheckingAccount=== tests
+     * the withdraw of an amount more than balance
      */
     @Test
     public void testSubFromBalance_DebitmMoreThanBalance()
@@ -148,10 +146,10 @@ public class CheckingAccountTest
         boolean result1 = instance.subFromBalance(200);
         assertEquals(expResult1, result1);
     }
-    
-      /**
-     * ===Test 2 of subFromeBalance method, of class CheckingAccount=== tests the
-     * withdraw of an amount less than daily limit
+
+    /**
+     * ===Test 2 of subFromeBalance method, of class CheckingAccount=== tests
+     * the withdraw of an amount less than daily limit
      */
     @Test
     public void testSubFromBalance_DebitLessThanDailyLimit()
@@ -162,10 +160,10 @@ public class CheckingAccountTest
         boolean result2 = instance.subFromBalance(5000);
         assertEquals(expResult2, result2);
     }
-    
+
     /**
-     * ===Test 3 of subFromeBalance method, of class CheckingAccount=== tests the
-     * withdraw of an amount equal to daily limit
+     * ===Test 3 of subFromeBalance method, of class CheckingAccount=== tests
+     * the withdraw of an amount equal to daily limit
      */
     @Test
     public void testSubFromBalance_debitEqualToDailyLimit()
@@ -175,28 +173,28 @@ public class CheckingAccountTest
         boolean expResult3 = true;
         boolean result3 = instance.subFromBalance(10000);
         assertEquals(expResult3, result3);
-    } 
-    
-        /**
-     * ===Test 4 of subFromeBalance method, of class CheckingAccount=== tests the
-     * withdraw of an amount more than daily limit
+    }
+
+    /**
+     * ===Test 4 of subFromeBalance method, of class CheckingAccount=== tests
+     * the withdraw of an amount more than daily limit
      */
     @Test
     public void testSubFromBalance_debitMoreThanDailyLimit()
-    {        
+    {
         System.out.println("Debit more than $10000 daily limit");
         boolean expResult4 = false;
         boolean result4 = instance.subFromBalance(5000);
         assertEquals(expResult4, result4);
-    } 
-    
-           /**
-     * ===Test 5 of subFromeBalance method, of class CheckingAccount=== tests the
-     * withdraw of an amount more than daily limit in multiple days period
+    }
+
+    /**
+     * ===Test 5 of subFromeBalance method, of class CheckingAccount=== tests
+     * the withdraw of an amount more than daily limit in multiple days period
      */
     @Test
     public void testSubFromBalance_debitMoreThanLimitInMultipleDays()
-    {   
+    {
         DateTimeUtils.setCurrentMillisOffset(0);
 
         DateTime real = new DateTime(DateTimeZone.forID("Etc/UTC"));
@@ -205,10 +203,9 @@ public class CheckingAccountTest
         instance.subFromBalance(5000);
         DateTimeUtils.setCurrentMillisOffset(100000000);
         DateTime fake = new DateTime(DateTimeZone.forID("Etc/UTC"));
-        
-        System.out.println("real: "+ real.toString());
-        System.out.println("fake: "+ fake.toString());
 
+        System.out.println("real: " + real.toString());
+        System.out.println("fake: " + fake.toString());
 
         boolean expResult5 = true;
         boolean result5 = instance.subFromBalance(6000);
